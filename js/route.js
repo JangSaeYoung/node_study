537ce46be4ab4d6fb005c8f3906a49f4282a099b
saeyoung <syoungbb25@gmail.com>
2022-09-02 14:40:39 +0900

    routed DB사용하기

--- js/route.js
+++ js/route.js
@@ -1,31 +1,19 @@
 //@ts-check
 
-/**
- * @typedef Post
- * @property {number} id
- * @property {string} title
- * @property {string} content
- */
+const fs = require('fs').promises;
 
-/**@type {Post[]} */
+async function getPosts() {
+  const jsonPosts = await fs.readFile('database.json', 'utf-8');
+  return JSON.parse(jsonPosts).posts;
+}
 
-const posts = [
-  {
-    id: 1,
-    title: '첫번째 블로그 글',
-    content: '첫번째 내용입니다.',
-  },
-  {
-    id: 2,
-    title: '두번째 블로그 글',
-    content: '두번째 내용입니다.',
-  },
-  {
-    id: 3,
-    title: '세번째 블로그 글',
-    content: '세번째 내용입니다.',
-  },
-];
+async function savePosts(posts) {
+  const content = {
+    posts,
+  };
+
+  return fs.writeFile('database.json', JSON.stringify(content), 'utf-8');
+}
 
 const routes = [
   //블로그 목록을 가져오는 API
@@ -33,20 +21,18 @@ const routes = [
     url: '/posts',
     method: 'GET',
     id: 'undefined',
-    callback: async () => ({
-      statusCode: 200,
-      body: {
-        posts: posts,
-        totalCount: posts.length,
-      },
-      // body: {
-      //   posts: posts.map((post) => ({
-      //     id: post.id,
-      //     title: post.title,
-      //   })),
-      //   totalCount: posts.length,
-      // },
-    }),
+    callback: async () => {
+      const posts = await getPosts();
+      const length = posts.length;
+
+      return {
+        statusCode: 200,
+        body: {
+          posts: await getPosts(),
+          totalCount: length,
+        },
+      };
+    },
   },
 
   // 특정 아이디의 블로그 글을 가져오는 API
@@ -55,6 +41,7 @@ const routes = [
     method: 'GET',
     id: 'number',
     callback: async (postId) => {
+      const posts = await getPosts();
       const id = postId;
       if (!id) {
         return {
@@ -84,11 +71,16 @@ const routes = [
     method: 'POST',
     id: 'undefined',
     callback: async (id, newPost) => {
+      const posts = await getPosts();
+
       posts.push({
         id: posts[posts.length - 1].id + 1,
         title: newPost.title,
         content: newPost.content,
       });
+
+      savePosts(posts);
+
       return {
         statusCode: 200,
         body: 'post is upload',
@@ -102,6 +94,8 @@ const routes = [
     method: 'PUT',
     id: 'number',
     callback: async (id, newPost) => {
+      const posts = await getPosts();
+
       if (!id) {
         return {
           statusCode: 404,
@@ -120,6 +114,9 @@ const routes = [
       // id 값이 없으므로 아래 코드로 id값을 주기
       modifyPost.id = id;
       posts[id - 1] = modifyPost;
+
+      savePosts(posts);
+
       return {
         statusCode: 200,
         body: modifyPost,
@@ -133,6 +130,8 @@ const routes = [
     method: 'DELETE',
     id: 'number',
     callback: async (id) => {
+      const posts = await getPosts();
+
       const result = posts.find((post) => post.id === id);
       if (!result) {
         return {
@@ -142,6 +141,9 @@ const routes = [
       }
 
       posts.splice(id - 1, 1);
+
+      savePosts(posts);
+
       return {
         statusCode: 200,
         body: 'post deleted',
@@ -153,3 +155,32 @@ const routes = [
 module.exports = {
   routes,
 };
+
+//posts를 변수로 사용
+// /**
+//  * @typedef Post
+//  * @property {number} id
+//  * @property {string} title
+//  * @property {string} content
+//  */
+
+// /**@type {Post[]} */
+
+// const posts = [
+//   {
+//     id: 1,
+//     title: '첫번째 블로그 글',
+//     content: '첫번째 내용입니다.',
+//   },
+//   {
+//     id: 2,
+//     title: '두번째 블로그 글',
+//     content: '두번째 내용입니다.',
+//   },
+//   {
+//     id: 3,
+//     title: '세번째 블로그 글',
+//     content: '세번째 내용입니다.',
+//   },
+// ];
+//posts를 변수로 사용 끝
